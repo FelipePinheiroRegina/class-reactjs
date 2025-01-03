bfd99c8c475836605231764639964bf837355e49
Felipe Pinheiro <flppinheiro@hotmail.com>
2025-01-02 21:58:41 -0300

    feat: add real requests on graphics charts of dashboard

--- pizza-shop/src/pages/app/dashboard/components/PopularProductsChart.tsx
+++ pizza-shop/src/pages/app/dashboard/components/PopularProductsChart.tsx
@@ -8,14 +8,8 @@ import {
     Pie,
     Cell
 } from 'recharts'
-
-const data = [
-    {product: 'Pizza Calabresa', amount: 40},
-    {product: 'Pizza Frango', amount: 100},
-    {product: 'Pizza Catupiry', amount: 64},
-    {product: 'Pizza Bauru', amount: 32},
-    {product: 'Pizza Gorgonzola', amount: 47},
-]
+import { useQuery } from '@tanstack/react-query'
+import { getPopularProducts } from '@/api/getPopularProducts'
 
 const COLORS = [
     colors.sky['500'],
@@ -26,6 +20,10 @@ const COLORS = [
 ]
 
 export function PopularProductsChart() {
+    const { data: popularProducts } = useQuery({
+        queryKey: ['metrics', 'popular-products'],
+        queryFn: getPopularProducts
+    })
     return (
         <Card className='col-span-3'>
             <CardHeader className='flex-row items-center justify-between pb-8'>
@@ -34,60 +32,62 @@ export function PopularProductsChart() {
             </CardHeader>
 
             <CardContent>
-                <ResponsiveContainer width='100%' height={240}>
-                    <PieChart style={{ fontSize: 12 }}>
-                        <Pie 
-                            dataKey='amount'
-                            nameKey='product'
-                            data={data}
-                            cx='50%'
-                            cy='50%'
-                            outerRadius={86}
-                            innerRadius={64}
-                            strokeWidth={8}
-                            labelLine={false}
-                            label={({
-                                cx,
-                                cy,
-                                midAngle,
-                                innerRadius,
-                                outerRadius,
-                                value,
-                                index,
-                              }) => {
-                                const RADIAN = Math.PI / 180
-                                const radius = 12 + innerRadius + (outerRadius - innerRadius)
-                                const x = cx + radius * Math.cos(-midAngle * RADIAN)
-                                const y = cy + radius * Math.sin(-midAngle * RADIAN)
-                              
-                                return (
-                                  <text
-                                    x={x}
-                                    y={y}
-                                    className="fill-muted-foreground text-xs"
-                                    textAnchor={x > cx ? 'start' : 'end'}
-                                    dominantBaseline="central"
-                                  >
-                                    {data[index].product.length > 12
-                                      ? data[index].product.substring(0, 12).concat('...')
-                                      : data[index].product}{' '}
-                                    ({value})
-                                  </text>
-                                )
-                            }}
-                        >
-                            {data.map((_, index) => {
-                                return (
-                                    <Cell 
-                                        key={`cell-${index}`} 
-                                        fill={COLORS[index]}
-                                        className='stroke-background hover:opacity-80'
-                                    />
-                                )
-                            })}
-                        </Pie>
-                    </PieChart>
+                { popularProducts && (
+                    <ResponsiveContainer width='100%' height={240}>
+                        <PieChart style={{ fontSize: 12 }}>
+                            <Pie 
+                                dataKey='amount'
+                                nameKey='product'
+                                data={popularProducts}
+                                cx='50%'
+                                cy='50%'
+                                outerRadius={86}
+                                innerRadius={64}
+                                strokeWidth={8}
+                                labelLine={false}
+                                label={({
+                                    cx,
+                                    cy,
+                                    midAngle,
+                                    innerRadius,
+                                    outerRadius,
+                                    value,
+                                    index,
+                                }) => {
+                                    const RADIAN = Math.PI / 180
+                                    const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                                    const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                                    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+                                
+                                    return (
+                                    <text
+                                        x={x}
+                                        y={y}
+                                        className="fill-muted-foreground text-xs"
+                                        textAnchor={x > cx ? 'start' : 'end'}
+                                        dominantBaseline="central"
+                                    >
+                                        {popularProducts[index].product.length > 12
+                                        ? popularProducts[index].product.substring(0, 12).concat('...')
+                                        : popularProducts[index].product}{' '}
+                                        ({value})
+                                    </text>
+                                    )
+                                }}
+                            >
+                                {popularProducts.map((_, index) => {
+                                    return (
+                                        <Cell 
+                                            key={`cell-${index}`} 
+                                            fill={COLORS[index]}
+                                            className='stroke-background hover:opacity-80'
+                                        />
+                                    )
+                                })}
+                            </Pie>
+                        </PieChart>
                 </ResponsiveContainer>
+                )}
             </CardContent>
         </Card>
     )
--- pizza-shop/src/pages/app/dashboard/components/RevenueChart.tsx
+++ pizza-shop/src/pages/app/dashboard/components/RevenueChart.tsx
@@ -9,18 +9,15 @@ import {
     CartesianGrid,
     Line
 } from 'recharts'
-
-const data = [
-    {date: '10/12', revenue: 1200},
-    {date: '11/12', revenue: 800},
-    {date: '12/12', revenue: 400},
-    {date: '13/12', revenue: 2300},
-    {date: '14/12', revenue: 600},
-    {date: '15/12', revenue: 1000},
-    {date: '16/12', revenue: 1700},
-]
+import { useQuery } from '@tanstack/react-query'
+import { getDailyRevenueInPeriod } from '@/api/getDailyRevenueInPeriod'
 
 export function RevenueChart() {
+    const { data: revenueInPeriod } = useQuery({
+        queryKey: ['metrics', 'daily-revenue-in-period'],
+        queryFn: getDailyRevenueInPeriod
+    })
+
     return (
         <Card className='col-span-6'>
             <CardHeader className='flex-row items-center justify-between pb-8'>
@@ -30,35 +27,38 @@ export function RevenueChart() {
                 </div>
             </CardHeader>
             <CardContent>
-                <ResponsiveContainer width='100%' height={240}>
-                    <LineChart data={data} style={{ fontSize: 12 }}>
-                        <XAxis 
-                            dataKey='date'
-                            tickLine={false}
-                            dy={16}
-                        />
+                { revenueInPeriod && (
+                    <ResponsiveContainer width='100%' height={240}>
+                        <LineChart data={revenueInPeriod} style={{ fontSize: 12 }}>
+                            <XAxis 
+                                dataKey='date'
+                                tickLine={false}
+                                dy={16}
+                            />
 
-                        <YAxis 
-                            stroke='#888' 
-                            axisLine={false} 
-                            tickLine={false} 
-                            width={80}
-                            tickFormatter={(value: number) => value.toLocaleString('pt-BR', { 
-                                style: 'currency',
-                                currency: 'BRL'
-                            })}
-                        />
-                         
-                        <CartesianGrid vertical={false} className='stroke-muted'/>
+                            <YAxis 
+                                stroke='#888' 
+                                axisLine={false} 
+                                tickLine={false} 
+                                width={80}
+                                tickFormatter={(value: number) => value.toLocaleString('pt-BR', { 
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                })}
+                            />
+                            
+                            <CartesianGrid vertical={false} className='stroke-muted'/>
 
-                        <Line 
-                            type='linear' 
-                            strokeWidth={2} 
-                            dataKey="revenue"
-                            stroke={colors.violet['500']}
-                        />
-                    </LineChart>
-                </ResponsiveContainer>
+                            <Line 
+                                type='linear' 
+                                strokeWidth={2} 
+                                dataKey="receipt"
+                                stroke={colors.violet['500']}
+                                
+                            />
+                        </LineChart>
+                    </ResponsiveContainer>
+                )}
             </CardContent>
         </Card>
     )

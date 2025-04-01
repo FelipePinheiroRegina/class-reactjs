938f543efcba81628be87ce48e7a3aaf8eee9637
Felipe Pinheiro <felipe.pinheiro@cotefacil.com>
2025-04-01 14:08:06 -0300

    feat: add some features on page schedule

---
+++ ignite-call/src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,49 @@
+import { prisma } from '@/lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+import dayjs from 'dayjs'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    res.status(405).end()
+    return
+  }
+
+  const username = String(req.query.username)
+  const { year, month } = req.query
+
+  if (!year || !month) {
+    res.status(400).json({ message: 'year and month is required.' })
+    return
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found' })
+    return
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  })
+
+  const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+    return !availableWeekDays.some(
+      (availableWeekDay) => availableWeekDay.week_day === weekDay,
+    )
+  })
+
+  res.status(200).json({ blockedWeekDays })
+}
--- ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -7,11 +7,47 @@ import {
   TimePickerList,
 } from './styles'
 import { Calendar } from '@/components/Calendar'
+import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
+import { api } from '@/lib/axios'
+import { useQuery } from '@tanstack/react-query'
+
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
+
+const QUERY_KEY_AVAILABILITY = 'availability'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const router = useRouter()
 
   const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
+
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
+
+  const { data: availability } = useQuery<Availability>({
+    queryKey: [QUERY_KEY_AVAILABILITY, selectedDateWithoutTime],
+    queryFn: async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: {
+          date: selectedDateWithoutTime,
+        },
+      })
+      return response.data
+    },
+    enabled: !!selectedDate,
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  })
 
   return (
     <Container isTimerPickerOpen={isDateSelected}>
@@ -20,21 +56,20 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            monday <span>march 20</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
+            {availability?.possibleTimes?.map((hour) => {
+              return (
+                <TimePickerItem
+                  key={hour}
+                  disabled={!availability.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, '0')}:00h
+                </TimePickerItem>
+              )
+            })}
           </TimePickerList>
         </TimePicker>
       )}
